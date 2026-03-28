a3a6bc81f1c20bb021bb0b1cd41d60cd709f43b9
franklin.barreto <franklin-barreto@hotmail.com>
2026-03-28 12:48:35 -0300

    first commit

---
+++ src/main/java/br/com/f2e/ovenplatform/OvenPlatformApplication.java
@@ -0,0 +1,13 @@
+package br.com.f2e.ovenplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OvenPlatformApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OvenPlatformApplication.class, args);
+  }
+
+}
---
+++ src/test/java/br/com/f2e/ovenplatform/OvenPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.f2e.ovenplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OvenPlatformApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
