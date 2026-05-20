db578e1e8865e899f2122a4110eb063fa8b59c6b
franklin.barreto <franklin-barreto@hotmail.com>
2026-05-20 15:12:47 -0300

    feat(identity): add tenant membership

---
+++ src/main/java/br/com/f2e/ovenplatform/identity/infrastructure/persistence/JpaTenantMembershipRepositoryAdapter.java
@@ -0,0 +1,28 @@
+package br.com.f2e.ovenplatform.identity.infrastructure.persistence;
+
+import br.com.f2e.ovenplatform.identity.application.TenantMembershipRepository;
+import br.com.f2e.ovenplatform.identity.domain.TenantMembership;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public class JpaTenantMembershipRepositoryAdapter implements TenantMembershipRepository {
+
+  private final SpringDataTenantMembershipRepository tenantMembershipRepository;
+
+  public JpaTenantMembershipRepositoryAdapter(
+      SpringDataTenantMembershipRepository tenantMembershipRepository) {
+    this.tenantMembershipRepository = tenantMembershipRepository;
+  }
+
+  @Override
+  public TenantMembership save(TenantMembership tenantMembership) {
+    return tenantMembershipRepository.save(tenantMembership);
+  }
+
+  @Override
+  public boolean existsByUserIdAndTenantId(UUID userId, UUID tenantId) {
+    return tenantMembershipRepository.existsByUserIdAndTenantId(userId, tenantId);
+  }
+}
