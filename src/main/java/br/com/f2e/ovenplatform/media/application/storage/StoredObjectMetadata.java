edd8dcbd6fe5f3979fbad98fa69c7904a2547b91
franklin.barreto <franklin-barreto@hotmail.com>
2026-07-27 15:47:06 -0300

    refactor(media): reuse positive size precondition

--- src/main/java/br/com/f2e/ovenplatform/media/application/storage/StoredObjectMetadata.java
+++ src/main/java/br/com/f2e/ovenplatform/media/application/storage/StoredObjectMetadata.java
@@ -1,15 +1,13 @@
 package br.com.f2e.ovenplatform.media.application.storage;
 
 import static br.com.f2e.ovenplatform.shared.domain.validation.Preconditions.requireNotBlank;
+import static br.com.f2e.ovenplatform.shared.domain.validation.Preconditions.requirePositive;
 
 public record StoredObjectMetadata(String contentType, long sizeBytes, String checksum) {
 
   public StoredObjectMetadata {
     contentType = requireNotBlank(contentType, "contentType");
+    requirePositive(sizeBytes, "sizeBytes");
     checksum = requireNotBlank(checksum, "checksum");
-
-    if (sizeBytes <= 0) {
-      throw new IllegalArgumentException("sizeBytes must be greater than zero");
-    }
   }
 }
