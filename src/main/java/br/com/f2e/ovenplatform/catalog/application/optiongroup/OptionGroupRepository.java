d26d7178206f116fb053adbc5580183551336709
Leandro Coutinho <leandro.torres.coutinho@gmail.com>
2026-08-03 20:02:11 -0300

    CTLG_223_2(2/X): pairs a real option group with a different existing tenant

--- src/main/java/br/com/f2e/ovenplatform/catalog/application/optiongroup/OptionGroupRepository.java
+++ src/main/java/br/com/f2e/ovenplatform/catalog/application/optiongroup/OptionGroupRepository.java
@@ -1,7 +1,7 @@
 package br.com.f2e.ovenplatform.catalog.application.optiongroup;
 
 import br.com.f2e.ovenplatform.catalog.domain.OptionGroup;
-import java.util.Collection;
+
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -9,8 +9,6 @@ import java.util.UUID;
 public interface OptionGroupRepository {
 
   OptionGroup save(OptionGroup optionGroup);
-
-  List<OptionGroup> saveAll(Collection<OptionGroup> optionGroups);
 
   Optional<OptionGroup> findByIdAndTenantIdAndProductId(UUID id, UUID tenantId, UUID productId);
 
